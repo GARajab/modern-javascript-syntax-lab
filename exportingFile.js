88359cc3724996486df06417a5cce4bf8098b24e
GARajab <mohamed.rajab@ewa.bh>
2024-12-04 23:27:08 +0300

    Done

---
+++ exportingFile.js
@@ -0,0 +1,5 @@
+export const name = "Matt"
+export const age = 43
+export const job = "programmer"
+
+export default { name, age, job }
---
+++ importingFile.js
@@ -0,0 +1,4 @@
+import * as data from "./exportingFile.js"
+console.log(data.name)
+console.log(data.age)
+console.log(data.job)
